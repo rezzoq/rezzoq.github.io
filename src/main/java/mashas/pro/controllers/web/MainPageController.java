c3a3f9cedca7afc523215c3819ba94bcaa9e484f
Masha <rezzo.qm@gmail.com>
2024-09-16 09:52:05 +0300

    start commit

---
+++ src/main/java/mashas/pro/controllers/web/MainPageController.java
@@ -0,0 +1,23 @@
+/*===================================================================================================*/
+/*                                                                    developer: Tretyakova Mariya;  */
+/*                                                                    version: 1.0;                  */
+/* Description:                                                                                      */
+/* Класс веб-контроллер для главной страницы.                                                          */
+/*===================================================================================================*/
+package mashas.pro.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}

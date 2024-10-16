823beabe08e6f2771706e4f7e438bb4e6ee848d3
Masha <rezzo.qm@gmail.com>
2024-10-16 11:45:14 +0300

    map final

--- src/main/resources/static/js/MapView.js
+++ src/main/resources/static/js/MapView.js
@@ -4,7 +4,7 @@ class MapView {
     /*===================================================================================================*/
     _div_name; // Элемент в HTML документе куда загружается карта
     _map_view; // Объект карты
-    _zoom_current = 15; // Текущий зум
+    _zoom_current = 12; // Текущий зум
     _zoom_max = 18; // Максимальный зум
     _zoom_min = 2; // Минимальный зум
     _start_longitude = 55.7273431; // Начальная долгота
@@ -18,12 +18,19 @@ class MapView {
             document.getElementById(this._div_name),
             {
                 center: new L.LatLng(this._start_longitude, this._start_latitude),
-                zoom: this._zoom_current
+                zoom: this._zoom_current,
+                attributionControl: false
             });
         L.featureGroup().addTo(this._map_view);
         this._source_map_cartoon_light.addTo(this._map_view);
         this.get_marker_shop(55.779119, 37.616451,
-            "../img/map/photo_shop_map_1.png", "Улица").addTo(this._map_view);
+            "../img/map/photo_shop_map_1.png", "улица Фонарная, 1").addTo(this._map_view);
+        this.get_marker_shop(55.641251, 37.635287,
+            "../img/map/photo_shop_map_2.png", "улица Ноября, 14").addTo(this._map_view);
+        this.get_marker_shop(55.742098, 37.5471497,
+            "../img/map/photo_shop_map_3.png", "улица Русаковского, 53").addTo(this._map_view);
+        this.get_marker_shop(55.745666, 37.649040,
+            "../img/map/photo_shop_map_4.png", "улица Открытий, 42к1").addTo(this._map_view);
 
     }
     /*===================================================================================================*/
@@ -49,7 +56,7 @@ class MapView {
             })
         });
         marker_shop.bindPopup(new L.popup().setContent(
-            '<img class="map_shop_img" src="' + img_src + '" alt=""/>'+
+            '<img class="map_shop_img" src="' + img_src + '" alt="" height="80px" width="80px"/><br>'+
         '<span class="map_shop_title">' + address + '</span>'));
         return marker_shop;
     }
